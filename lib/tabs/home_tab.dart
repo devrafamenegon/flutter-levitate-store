9a93a07f47943918c0aa30a403911dac237d5f87
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-07 18:53:11 -0300

    System of grid images

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
 
 class HomeTab extends StatelessWidget {
   @override
@@ -34,6 +38,41 @@ class HomeTab extends StatelessWidget {
                 title: Text("Novidades", style: TextStyle(color: Colors.white)),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+                future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
+                builder: (context, snapshot){
+                  if(!snapshot.hasData)
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      ),
+                  );
+                else
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map(
+                        (doc){
+                          return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                        }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                        (doc){
+                          return FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: doc.data["image"],
+                            fit: BoxFit.cover,
+                          );
+                        }
+                    ).toList(),
+                  );
+              },
             )
           ],
         )
