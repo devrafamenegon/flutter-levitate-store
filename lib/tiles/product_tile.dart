2b06e3414a8833297d78700598490a9b461fefb5
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-09 22:32:46 -0300

    Creating grid tile

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,15 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_levitate/datas/product_data.dart';
 
 class ProductTile extends StatelessWidget {
 
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == "grid" ?
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 0.8,
+                child: Image.network(
+                  product.images[0],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500
+                            ),
+                        ),
+                        Text(
+                          "R\$ ${product.price.toStringAsFixed(2)}",
+                          style: TextStyle(color: Color.fromARGB(255, 74, 101, 114),
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+              ),
+            ],
+          )
+          : Row()
+      ),
+    );
   }
 }
