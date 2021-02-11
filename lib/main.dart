ebf124965264e0ab8fd5d4f963422b0699ce23e3
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-11 16:02:02 -0300

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_levitate/screens/home_screen.dart';
+import 'package:flutter_levitate/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         // primaryColor: Color.fromARGB(255, 4, 125, 141),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
