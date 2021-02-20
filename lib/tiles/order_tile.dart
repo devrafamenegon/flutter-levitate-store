eb4c013752019000131a5a36b0bac111f26423e3
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-20 14:06:59 -0300

    Card order creation

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
@@ -9,7 +10,29 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(orderId),
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: StreamBuilder<DocumentSnapshot>(
+          //usamos o .snapshots pois ele retorna em tempo real um stream, ja o .get retornaria um FutureBuilder
+          stream: Firestore.instance.collection("order").document(orderId).snapshots(),
+          builder: (context, snapshot){
+            if(!snapshot.hasData){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Column(
+                children: [
+                  Text("Código do pedido: ${snapshot.data.documentID}",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
