a78a69f8a82e240359bb5aa5d8b28b9742fed457
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-20 17:10:17 -0300

    Card store creation

--- lib/tiles/place_tile.dart
+++ lib/tiles/place_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PlaceTile extends StatelessWidget {
 
@@ -9,6 +10,59 @@ class PlaceTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 100.0,
+            child: Image.network(snapshot.data["image"], fit: BoxFit.cover,),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  snapshot.data["title"],
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17.0,
+                  ),
+                ),
+                Text(
+                  snapshot.data["address"],
+                  textAlign: TextAlign.start,
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(
+                child: Text("Ver no Mapa"),
+                textColor: Color.fromARGB(255, 74, 101, 114),
+                padding: EdgeInsets.zero,
+                onPressed: (){
+                  launch("https://www.google.com/maps/search/?api=1&query=${snapshot.data["lat"]},${snapshot.data["long"]}");
+                },
+              ),
+              FlatButton(
+                child: Text("Ligar"),
+                textColor: Color.fromARGB(255, 249, 170, 51),
+                padding: EdgeInsets.zero,
+                onPressed: (){
+                  //abrir o app para ligar
+                  launch("tel:${snapshot.data["phone"]}");
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
